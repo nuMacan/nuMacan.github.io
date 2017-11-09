5503fb3fe6410dbae4ab31edb741255f1e5fcb36
Nu <nunung.khotimah@ymail.com>
2017-11-09 16:54:28 +0800

    second update for java script and query

---
+++ myjava.js
@@ -0,0 +1,15 @@
+ $(document).ready(function() {
+  $('#navy-btn').on('click', function() {
+    $('body').css('background', '#000080')
+  })
+  
+  $('#pink-btn').on('click', function() {
+    $('body').css('background', '#FFC0CB')
+  })
+  
+  $('#white-btn').on('click', function() {
+    $('body').css('background', '#FFFFFF')
+  })
+})
+
+
